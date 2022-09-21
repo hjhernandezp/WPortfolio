88e979f7d3b68d1e3a12201e05ed94a503514466
Héctor Hernández Petit <hjhernandezp@gmail.com>
2022-09-21 18:47:10 -0300

    About, Tabs y Panel
    Desarrollo de 3 componentes.
    About: corresponde al área Acerca de mi.
    Tabs: pestañas para selección de las partes del CV.
    Panel: contiene las partes del CV: Experiencia, Estudios, Habilidades, ..., ...
    
    No desarrollé un componente por cada parte del CV, al no considerarlo necesario en concordancia a la forma que decidí presentarlas.

--- WPortfolioFront/src/app/app.module.ts
+++ WPortfolioFront/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent,
+    TabsComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
